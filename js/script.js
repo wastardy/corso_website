a37c1dec4853f00d4a590c59b698e73c9f9fdb36
Andrew Kononenko <wastardy.k@gmail.com>
2024-12-07 21:30:02 +0200

    add script for navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-// Add custom JavaScript here
+function userScroll() {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('bg-dark', 'bg-opacity');
+        }
+        else {
+            navbar.classList.remove('bg-dark', 'bg-opacity')
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', userScroll);
