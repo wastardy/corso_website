241f9c2c871be677b7e8ff627a405b9adcf8c958
Andrew Kononenko <wastardy.k@gmail.com>
2024-12-08 15:34:23 +0200

    add script to close navbar list om small screen

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@ function userScroll() {
 }
 
 document.addEventListener('DOMContentLoaded', userScroll);
+
+document.addEventListener('click', function (event) {
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const navbarCollapse = document.querySelector('#navbar-nav-dropdown');
+
+    if (
+        navbarCollapse.classList.contains('show') &&
+        !navbarCollapse.contains(event.target) &&
+        !navbarToggler.contains(event.target)
+    ) {
+        // imitation of 'click on the 
+        // button' for close navbar list
+        navbarToggler.click(); 
+    }   
+});
